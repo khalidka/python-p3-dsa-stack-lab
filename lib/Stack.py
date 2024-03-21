667f2ffbdd4224021d15c584741b9e6a2f02424a
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-03-21 17:07:10 +0300

    feat:pass the tests

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,49 @@
 class Stack:
-
-    def __init__(self, items = [], limit = 100):
-        pass
-
+    limit = 100
+    def __init__(self, items = [], limit = None):
+        self.items = items
+        self.limit = limit
+        
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
+
 
     def push(self, item):
-        pass
+        if self.limit is None or len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            print("Stack is full")
+
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            return None
 
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            return None
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return self.limit is not None and len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        try:
+            index = self.items.index(target)
+            return self.size() - index - 1
+        except ValueError:
+            return -1
+
+# stk = Stack([1,2,3,4,5])
+# print(stk.size())
+    
+stk = Stack([1], 1)    
+stk.push(1)
+stk.push(2)
+print(stk.full())
